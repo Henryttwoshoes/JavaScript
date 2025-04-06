042fe8788a2fb1d980468425104db566b2e21728
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-05 21:36:43 -0300

    Update ambiente.js

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -1,9 +1,11 @@
+function ambiente() {
 let num =[5,8,3,9,3]
 num.push(1)
 num.sort()
 for (o = 0; o < num.length; o++){
 console.log(num[o])
-}
+}}
+
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
@@ -13,3 +15,5 @@ console.log(`O valor 8 está na posição ${pos}`)
 } else {
     console.log('O valor não foi encontrado')
 }
+
+
