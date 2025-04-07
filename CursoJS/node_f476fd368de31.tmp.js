6c4026eb33146ec56c886297e53185fa7cac06c1
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-07 12:47:02 -0300

    A

---
+++ CursoJS/node_f476fd368de31.tmp.js
@@ -0,0 +1,14 @@
+let valores = [8,1,7,4,2,9]
+valores.sort()
+
+
+setInterval(() => {
+    function ordem() {
+        console.log(valores)
+    
+        return valores
+    }
+    console.log(valores)
+}, 3000);
+
+clearInterval
--- CursoJS/teste01.js
+++ CursoJS/teste01.js
@@ -1,4 +1,23 @@
-let valores = [8, 1,7,4,2,9]
+let valores = [8,1,7,4,2,9]
 valores.sort()
-console.log(valores)
 
+
+setInterval(() => {
+    function ordem() {
+        console.log(valores)
+    
+        return valores
+    }
+    console.log(valores)
+}, 1500);
+
+
+let counter = 0
+var countinterval = setInterval(counter, 1000);
+function count() {
+    console.log(counter++);
+
+    if (counter > 5){
+        clearInterval(ordem)
+    }
+}
