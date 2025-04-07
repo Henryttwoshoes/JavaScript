0226c4cc796a0a92ad13ef08a78fdb5abf6e46ff
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-07 12:13:46 -0300

    Update teste01.js

--- CursoJS/teste01.js
+++ CursoJS/teste01.js
@@ -1,6 +1,10 @@
 let valores = [8, 1,7,4,2,9]
 valores.sort()
 console.log(valores)
-for (c = 3; c > 1; c--)
-    valores.push(c)
-console.log(valores)
+
+function mensagem(){
+    for (c = 3 ; c > 1; c--)
+    console.log(valores)
+}
+
+setTimeout(mensagem, 3000)
