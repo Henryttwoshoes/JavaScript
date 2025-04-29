a793c105d6782f44ab16be17571d143793a56e16
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-29 15:00:42 -0300

    comecei o rascunho de um próximo exercício

---
+++ Projetos/Questionário de perguntas objetivas/teste.js
@@ -0,0 +1,19 @@
+
+let atual = 1
+let corretas = 0
+
+
+const quiz = [{
+pergunta: "Quanto é 1 + 1?",
+alternativas: ["2","3","4","5"],
+correta: 1
+},
+{
+pergunta: "Quanto é 2 + 1?",
+alternativas: ["4","3","5","1"],
+correta: 2
+}
+]
+
+
+console.log(quiz[0])
